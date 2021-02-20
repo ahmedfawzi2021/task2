354545cb15e94bfe975e86a88d4b3f59a7bdbc46
ahmedfawzi2021 <79059325+ahmedfawzi2021@users.noreply.github.com>
2021-02-20 16:43:39 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# In[9]:
+
+
+a = float(input ("Input the length of the rectangle : "))
+b = float(input ("Input the length of the width : "))
+
+Perimeter = 2*(a+ b)
+print("Perimeter of rectangle is: ");
+print(Perimeter);
+
+
+# In[ ]:
+
+
+
+
